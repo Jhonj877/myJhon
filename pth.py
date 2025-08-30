c9e9bf6fa3b8d627b8c1a6c38559fa01d4602ba5
jhon <jaidercruz2009@gmail.com>
2025-08-30 11:39:39 -0500

    holaa

---
+++ pth.py
@@ -0,0 +1,10 @@
+
+numero1 = 15
+numero2 = 2
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(numero1 ** numero2)
+print(numero1 % numero2)
